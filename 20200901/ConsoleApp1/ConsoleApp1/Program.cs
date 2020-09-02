9e6040a10f7a669a403077e9720f785f6796e7f6
florencia.marinio@davinci.edu.ar <florencia.marino@davinci.edu.ar>
2020-09-01 21:20:46 -0300

    ej

--- 20200901/ConsoleApp1/ConsoleApp1/Program.cs
+++ 20200901/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,38 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+			
+			int intento = 3;
+			int entrada;
+			byte i, adivinado;
+			string linea;
+
+	
+			Random random = new Random();
+			int numero = random.Next(20);
+
+			i = 1; adivinado = 0;
+			do
+			{
+				Console.Write("Ingrese un numero entre 1 y 20: ");
+				linea = Console.ReadLine();
+				entrada = int.Parse(linea);
+
+				if ((entrada == numero))
+				{
+					Console.WriteLine("Adivinaste el numero!");
+					adivinado = 1;
+				}
+				
+				i++;
+			} 
+			while (((i <= intento) & (adivinado == 0)));
+			if ((adivinado == 0))
+			{
+				Console.Write("No adivinaste, el numero era: " + numero);
+			}
+
+			
+		}
     }
 }
