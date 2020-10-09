f9eac3207c266f7cb8fd6bdc8b8fb9a83db6d225
florencia.marinio <florencia.marinio@davinci.edu.ar>
2020-10-09 12:01:39 -0300

    parcial

---
+++ Parcial/ConsoleApp1/ConsoleApp1/TareasDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class TareasDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = tareas.db");
+        }
+    }
+}
